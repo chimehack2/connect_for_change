0aec58549a56c1d98d6c3e0332a6a8a501381094
Diana Hilton <diana.hilton@gmail.com>
2015-07-11 11:16:10 -0700

    Create nav bar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_bar_partial
+    # if current_user && current_user.administrator? && user_signed_in?
+      # 'layouts/logged_in_user_nav_admin'
+    if user_signed_in?
+      'layouts/logged_in_user_nav'
+    else
+      'layouts/guest_user_nav'
+    end
+  end
 end
