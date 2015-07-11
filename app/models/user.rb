4ee23387f48a4d9e811f8de4557a64352499fd82
Jack Chou <jackshchou@gmail.com>
2015-07-11 14:53:20 -0700

    Restore user signup role validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :role
   ADMINISTRATOR = 'administrator'
   MENTOR = 'mentor'
   MENTEE = 'mentee'
