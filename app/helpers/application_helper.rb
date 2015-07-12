d0d1ba51e81f26b24b0adce5c7c829c589c2c0b0
Jack Chou <jackshchou@gmail.com>
2015-07-11 18:27:57 -0700

    Add landing page partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,32 @@ module ApplicationHelper
       'layouts/guest_user_nav'
     end
   end
+  def footer_partial
+    unless current_user && user_signed_in?
+      'layouts/footer'
+    else
+      'layouts/blank'
+    end
+  end
+  def landing_accordion_partial
+    unless current_user && user_signed_in?
+      'layouts/landing_accordion'
+    else
+      'layouts/blank'
+    end
+  end
+  def landing_carousel_partial
+    unless current_user && user_signed_in?
+      'layouts/landing_carousel'
+    else
+      'layouts/blank'
+    end
+  end
+  def landing_top_bar_partial
+    unless current_user && user_signed_in?
+      'layouts/landing_top_bar'
+    else
+      'layouts/blank'
+    end
+  end
 end
